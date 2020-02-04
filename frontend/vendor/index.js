4b211cf518a10e0c9ac79716fa9a41f580eda6be
Victor Yunenko <victor@what.digital>
2020-02-04 09:27:30 +0100

    try to fix the fonts

--- frontend/vendor/index.js
+++ frontend/vendor/index.js
@@ -1,1 +1,6 @@
 require('bootstrap/scss/bootstrap.scss');
+
+require('@fortawesome/fontawesome-free/scss/fontawesome.scss');
+require('@fortawesome/fontawesome-free/scss/brands.scss');
+require('@fortawesome/fontawesome-free/scss/solid.scss');
+require('@fortawesome/fontawesome-free/scss/regular.scss');
